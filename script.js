351b99830cb0c798e2bb1e87af484eed362a5294
Marina-Kr <61282249+Marina-Kr@users.noreply.github.com>
2020-08-27 23:29:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 document.onkeydown = async function (e) {
     let audio = document.createElement('audio')
-    audio.src=`assets/${e.key}.wav`;
-    audio.autoplay=true;
+    audio.src = `assets/${e.key}.wav`;
+    audio.autoplay = true;
 }
 
-function letMusicBe (elem) {
+function letMusicBe(elem) {
     let audio = document.createElement('audio')
-    audio.src=`assets/${this.innerHTML}.wav`;
-    audio.autoplay=true;
+    audio.src = `assets/${this.innerHTML}.wav`;
+    audio.autoplay = true;
 }
 
-    window.onload = async function () {
+window.onload = async function () {
     let keys = document.querySelectorAll('kbd')
-        keys.forEach((elem) => {
+    keys.forEach((elem) => {
         elem.addEventListener('click', letMusicBe.bind(elem))
-        })
-    }
+    })
+}
 
 
 
